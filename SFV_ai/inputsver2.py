7ce9d84a77a2fc652e12d2e71253ee6622eda0bf
cdesilv1 <chris.desilva_@outlook.com>
2018-12-28 16:36:46 -0700

    added final actions to inputsver2

--- SFV_ai/inputsver2.py
+++ SFV_ai/inputsver2.py
@@ -9,9 +9,9 @@ SendInput = ctypes.windll.user32.SendInput
 
 
 
-#############################SF V Actions#############################
 
 #############################Movement#############################
+
 UP = 0x11 #W
 LEFT = 0x1E #A
 DOWN = 0x1F #S
@@ -34,8 +34,30 @@ HK = 0x32 #heavy kick
 HP = 0x24 #heavy punch
 AK = 0x33 #all kick
 AP = 0x25 #all punch
+THROW = [LP, LK]
+
+#############################Directional Attacks#############################
+
+LEFT_MP = [LEFT, MP] # Zugaihasatsu
+RIGHT_MP = [RIGHT, MP] # Zugaihasatsu
+
+LEFT_HP = [LEFT, HP] # Kikokuduki
+RIGHT_HP = [RIGHT, HP] # Kikokuduki
+DOWN_HP = [DOWN, HP]
+
+DOWN_MK = [DOWN, MK] # Tenmakujinkyaku
 
 
+
+
+
+
+
+#############################Miscellaneous Actions#############################
+
+VTRIGGER = [HP, HK]
+
+CHARGED_ATK = [MP, MK]
 
 def do_nothing():
     time.sleep(0.4)
@@ -93,7 +115,8 @@ ctypes.pointer(extra) )
 
 
 
-def executeAction(action, duration=0.05):
+
+def executeAction(action, duration=0.015): # Execute Keypresses
     if type(action) is int:
         pressKey(action)
         time.sleep(duration)
@@ -110,21 +133,246 @@ def executeAction(action, duration=0.05):
         raise TypeError('action needs to be an int or list')
 
 
-if __name__ == '__main__':
-    movements = [UP, LEFT, RIGHT, DOWN]
-    durations = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
-    attacks = [LK, MK, HK, LP, MP, HP]
-    time.sleep(8)
-    while True:
-        try:
-            current_movement = movements[random.randint(0, len(movements))]
-            executeAction(current_movement)
-        except:
-            do_nothing()
-        try:
-            current_attack = attacks[random.randint(0, len(attacks))]
-            executeAction(current_attack)
-        except:
-            do_nothing()
+#############################Advanced Movement#############################
 
-        time.sleep(0.05)
+def QCR(DOWN=DOWN, RIGHT=RIGHT): # Quarter Circle Right
+    pressKey(DOWN)
+    time.sleep(0.015)
+    pressKey(RIGHT)
+    time.sleep(0.015)
+    releaseKey(DOWN)
+    time.sleep(0.015)
+    releaseKey(RIGHT)
+    time.sleep(0.015)
+
+
+def QCL(DOWN=DOWN, LEFT=LEFT): # Quarter Circle Left
+    pressKey(DOWN)
+    time.sleep(0.015)
+    pressKey(LEFT)
+    time.sleep(0.015)
+    releaseKey(DOWN)
+    time.sleep(0.015)
+    releaseKey(LEFT)
+    time.sleep(0.015)
+
+def HCR(LEFT=LEFT, DOWN=DOWN, RIGHT=RIGHT): # Half Circle Right
+    pressKey(LEFT)
+    time.sleep(0.015)
+    pressKey(DOWN)
+    time.sleep(0.015)
+    releaseKey(LEFT)
+    time.sleep(0.015)
+    pressKey(RIGHT)
+    time.sleep(0.015)
+    releaseKey(DOWN)
+    time.sleep(0.015)
+    releaseKey(RIGHT)
+    time.sleep(0.015)
+
+
+def HCL(LEFT=LEFT, DOWN=DOWN, RIGHT=RIGHT): # Half Circle Left
+    pressKey(RIGHT)
+    time.sleep(0.015)
+    pressKey(DOWN)
+    time.sleep(0.015)
+    releaseKey(RIGHT)
+    time.sleep(0.015)
+    pressKey(LEFT)
+    time.sleep(0.015)
+    releaseKey(DOWN)
+    time.sleep(0.015)
+    releaseKey(LEFT)
+    time.sleep(0.015)
+
+def DASH_LEFT(LEFT=LEFT):
+    pressKey(LEFT)
+    releaseKey(LEFT)
+    time.sleep(0.015)
+    pressKey(LEFT)
+    releaseKey(LEFT)
+
+def DASH_RIGHT(RIGHT=RIGHT):
+    pressKey(RIGHT)
+    releaseKey(RIGHT)
+    time.sleep(0.015)
+    pressKey(RIGHT)
+    releaseKey(RIGHT)
+
+
+#############################Advanced Attacks#############################
+
+def HADOKEN_LEFT():
+    QCL()
+    executeAction(LP)
+
+def HADOKEN_RIGHT():
+    QCR()
+    executeAction(LP)
+
+def SHAKUNETSU_HADOKEN_LEFT():
+    HCL()
+    executeAction(LP)
+
+def SHAKUNETSU_HADOKEN_RIGHT():
+    HCR()
+    executeAction(LP)
+
+def SHORYUKEN_LEFT(LEFT=LEFT, DOWN=DOWN):
+    pressKey(LEFT)
+    time.sleep(0.015)
+    releaseKey(LEFT)
+    time.sleep(0.015)
+    pressKey(DOWN)
+    time.sleep(0.015)
+    releaseKey(DOWN)
+    time.sleep(0.015)
+    pressKey(LEFT)
+    pressKey(DOWN)
+    time.sleep(0.015)
+    pressKey(LP)
+    releaseKey(DOWN)
+    releaseKey(LEFT)
+    releaseKey(LP)
+
+def SHORYUKEN_RIGHT(RIGHT=RIGHT, DOWN=DOWN):
+    pressKey(RIGHT)
+    time.sleep(0.015)
+    releaseKey(RIGHT)
+    time.sleep(0.015)
+    pressKey(DOWN)
+    time.sleep(0.015)
+    releaseKey(DOWN)
+    time.sleep(0.015)
+    pressKey(RIGHT)
+    pressKey(DOWN)
+    time.sleep(0.015)
+    pressKey(LP)
+    releaseKey(DOWN)
+    releaseKey(RIGHT)
+    releaseKey(LP)
+
+def KUREKIJIN_LEFT():
+    QCL()
+    executeAction(LK)
+
+def KUREKIJIN_RIGHT():
+    QCR()
+    executeAction(LK)
+
+def RYUSOKYAKU_LEFT():
+    HCL()
+    executeAction(LK)
+
+def RYUSOKYAKU_RIGHT():
+    HCR()
+    executeAction(LK)
+
+def METSU_SHORYUKEN_LEFT():
+    QCL()
+    QCL()
+    executeAction(LP)
+
+def METSU_SHORYUKEN_RIGHT():
+    QCR()
+    QCR()
+    executeAction(LP)
+
+def SHORT_SPINKICK_LEFT():
+    QCL()
+    executeAction(LK)
+
+def SHORT_SPINKICK_RIGHT():
+    QCR()
+    executeAction(LK)
+
+def LONG_SPINKICK_LEFT():
+    QCL()
+    executeAction(MK)
+
+def LONG_SPINKICK_RIGHT():
+    QCR()
+    executeAction(MK)
+
+
+#############################__main__#############################
+
+if __name__ == '__main__':
+    '''
+    For the case of the randomized actions, actions will be chosen from a distribution as follows: movemements: 20%, attacks: 45%, special_attacks: 25%, trigger_moves: 5%, do_nothing: 5%
+    movement durations will be chosen uniformly, and once a category is chosen, actions will be chosen uniformly
+    randomized action category PDF was chosen arbitrarily
+    '''
+
+    movements = [UP, LEFT, RIGHT, DOWN, UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT, 'DASH_LEFT', 'DASH_RIGHT']
+
+    durations = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8]
+
+    attacks = [LK, MK, HK, LP, MP, HP, LEFT_MP, RIGHT_MP, LEFT_HP, RIGHT_HP, DOWN_HP, THROW]
+
+    special_attacks = [CHARGED_ATK, 'HADOKEN_LEFT', 'HADOKEN_RIGHT', 'SHAKUNETSU_HADOKEN_LEFT', 'SHAKUNETSU_HADOKEN_RIGHT', 'SHORYUKEN_LEFT',\
+    'SHORYUKEN_RIGHT', 'KUREKIJIN_LEFT', 'KUREKIJIN_RIGHT', 'RYUSOKYAKU_LEFT', 'RYUSOKYAKU_RIGHT', 'SHORT_SPINKICK_LEFT',\
+     'SHORT_SPINKICK_RIGHT', 'LONG_SPINKICK_LEFT', 'LONG_SPINKICK_RIGHT']
+
+    trigger_moves = [VTRIGGER, 'METSU_SHORYUKEN_LEFT', 'METSU_SHORYUKEN_RIGHT']
+
+    time.sleep(8)
+
+    while True:
+        choose_action = random.random(0, 101)
+        if choose_action <=20:
+            choice = random.choice(movements)
+            if type(choice) is str:
+                if choice == 'DASH_LEFT':
+                    DASH_LEFT()
+                else:
+                    DASH_RIGHT()
+            else:
+                executeAction(choice, random.choice(durations))
+        elif choose_action >20 and choose_action <=65:
+            choice = random.choice(attacks)
+            executeAction(choice)
+        elif choose_action >65 and choose_action <=90:
+            choice = random.choice(special_attacks)
+            if type(choice) is str:
+                if choice == 'HADOKEN_LEFT':
+                    HADOKEN_LEFT()
+                elif choice == 'HADOKEN_RIGHT':
+                    HADOKEN_RIGHT()
+                elif choice == 'SHAKUNETSU_HADOKEN_LEFT':
+                    SHAKUNETSU_HADOKEN_LEFT()
+                elif choice == 'SHAKUNETSU_HADOKEN_RIGHT':
+                    SHAKUNETSU_HADOKEN_RIGHT()
+                elif choice == 'SHORYUKEN_LEFT':
+                    SHORYUKEN_LEFT()
+                elif choice == 'SHORYUKEN_RIGHT':
+                    SHORYUKEN_RIGHT()
+                elif choice == 'KUREKIJIN_LEFT':
+                    KUREKIJIN_LEFT()
+                elif choice == 'KUREKIJIN_RIGHT':
+                    KUREKIJIN_RIGHT()
+                elif choice == 'RYUSOKYAKU_LEFT':
+                    RYUSOKYAKU_LEFT()
+                elif choice == 'RYUSOKYAKU_RIGHT':
+                    RYUSOKYAKU_RIGHT()
+                elif choice == 'SHORT_SPINKICK_LEFT':
+                    SHORT_SPINKICK_LEFT()
+                elif choice == 'SHORT_SPINKICK_RIGHT':
+                    SHORT_SPINKICK_RIGHT()
+                elif choice == 'LONG_SPINKICK_LEFT':
+                    LONG_SPINKICK_LEFT()
+                else:
+                    LONG_SPINKICK_RIGHT()
+            else:
+                executeAction(choice)
+        elif choose_action >90 and choose_action <=95:
+            choice = random.choice(trigger_moves)
+            if type(choice) is str:
+                if choice == 'METSU_SHORYUKEN_LEFT':
+                    METSU_SHORYUKEN_LEFT()
+                else:
+                    METSU_SHORYUKEN_RIGHT()
+            else:
+                executeAction(choice)
+        else:
+            do_nothing()
