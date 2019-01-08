f20b70bea3849b92c286e356425dac7d2fe2de0b
cdesilv1 <chris.desilva_@outlook.com>
2019-01-08 11:13:45 -0700

    modified _keylogger

--- SFV_ai/_keylogger.py
+++ SFV_ai/_keylogger.py
@@ -1,4 +1,5 @@
 import pythoncom, pyHook
+from pyHook import KeyboardEvent  
 
 def OnKeyboardEvent(event):
     # print('MessageName:',event.MessageName)
@@ -9,7 +10,7 @@ def OnKeyboardEvent(event):
     # print('Ascii:', event.Ascii, chr(event.Ascii))
     # print('Key:', event.Key)
     # print('KeyID:', event.KeyID)
-    return event.ScanCode
+    # print('ScanCode:', event.ScanCode)
     # print('Extended:', event.Extended)
     # print('Injected:', event.Injected)
     # print('Alt', event.Alt)
@@ -17,13 +18,17 @@ def OnKeyboardEvent(event):
     # print('---')
 
 # return True to pass the event to other handlers
-    # return True
+    return True
+
+def KeybdEvent(event):
+    return KeyboardEvent.GetKey(event)
 
 if __name__ == '__main__':
     # create a hook manager
     hm = pyHook.HookManager()
     # watch for all mouse events
     hm.KeyDown = OnKeyboardEvent
+    print(KeybdEvent)
     # set the hook
     hm.HookKeyboard()
     # wait forever
