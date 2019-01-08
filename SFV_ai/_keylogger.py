0a1c02f7f641af36cffdfab56d80279bf7c42828
cdesilv1 <chris.desilva_@outlook.com>
2019-01-08 11:40:06 -0700

    changed _keylogger

--- SFV_ai/_keylogger.py
+++ SFV_ai/_keylogger.py
@@ -1,32 +1,38 @@
 import pythoncom, pyHook
 from pyHook import KeyboardEvent  
 
-def OnKeyboardEvent(event):
-    # print('MessageName:',event.MessageName)
-    # print('Message:',event.Message)
-    # print('Time:',event.Time)
-    # print('Window:',event.Window)
-    # print('WindowName:',event.WindowName)
-    # print('Ascii:', event.Ascii, chr(event.Ascii))
-    # print('Key:', event.Key)
-    # print('KeyID:', event.KeyID)
-    # print('ScanCode:', event.ScanCode)
-    # print('Extended:', event.Extended)
-    # print('Injected:', event.Injected)
-    # print('Alt', event.Alt)
-    # print('Transition', event.Transition)
-    # print('---')
-    print (event.GetKey())
+class KeyLogger(object):
+    def __init__(self):
+        self.current_key = None
 
-# return True to pass the event to other handlers
-    return True
+    def OnKeyboardEvent(self, event):
+        # print('MessageName:',event.MessageName)
+        # print('Message:',event.Message)
+        # print('Time:',event.Time)
+        # print('Window:',event.Window)
+        # print('WindowName:',event.WindowName)
+        # print('Ascii:', event.Ascii, chr(event.Ascii))
+        # print('Key:', event.Key)
+        # print('KeyID:', event.KeyID)
+        # print('ScanCode:', event.ScanCode)
+        # print('Extended:', event.Extended)
+        # print('Injected:', event.Injected)
+        # print('Alt', event.Alt)
+        # print('Transition', event.Transition)
+        # print('---')
+        self.current_key = event.GetKey()
+
+    # return True to pass the event to other handlers
+        return True
 
 
 if __name__ == '__main__':
     # create a hook manager
     hm = pyHook.HookManager()
     # watch for all mouse events
-    hm.KeyDown = OnKeyboardEvent
+    keylogger = KeyLogger()
+    hm.KeyDown = keylogger.OnKeyboardEvent
+    print (keylogger.current_key)
     # set the hook
     hm.HookKeyboard()
     # wait forever
