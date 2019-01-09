04fe9173363871bc8a6edb883a474f74be2b4371
cdesilv1 <chris.desilva_@outlook.com>
2019-01-09 16:27:22 -0700

    changed data_capture

--- SFV_ai/data_capture.py
+++ SFV_ai/data_capture.py
@@ -8,7 +8,7 @@ def screen_record():
         # 800x600 windowed mode
         printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
         print(printscreen.shape)
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        # cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('x'):
             cv2.destroyAllWindows()
             break
