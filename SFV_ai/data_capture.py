b10db0e6adb9e6a4eecfa1e2e5fcb26ba27b68f9
cdesilv1 <chris.desilva_@outlook.com>
2019-01-09 17:16:09 -0700

    changed pillow ImageGrab to greyscale

--- SFV_ai/data_capture.py
+++ SFV_ai/data_capture.py
@@ -3,10 +3,11 @@ from PIL import ImageGrab
 import cv2
 import time
 
-def screen_record(): 
+def screen_record():
+    all_data = np.array()
     while(True):
         # 800x600 windowed mode
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)).convert('L'))
         print(printscreen.shape)
         # cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('x'):
