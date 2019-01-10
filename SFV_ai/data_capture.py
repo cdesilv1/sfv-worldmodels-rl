afb687621631bf87ae47ef0cc08200de6c586e2f
cdesilv1 <chris.desilva_@outlook.com>
2019-01-10 10:23:28 -0700

    changed data_capture

--- SFV_ai/data_capture.py
+++ SFV_ai/data_capture.py
@@ -8,7 +8,6 @@ def screen_record():
     while(True):
         # 800x600 windowed mode
         printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)).convert('L'))
-        print(printscreen.shape)
         # cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('x'):
             cv2.destroyAllWindows()
