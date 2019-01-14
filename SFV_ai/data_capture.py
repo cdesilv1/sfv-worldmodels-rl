9155c1fb9872139677cf7d4861f000ae29165e57
cdesilv1 <chris.desilva_@outlook.com>
2019-01-14 12:45:51 -0700

    changed data_capture

--- SFV_ai/data_capture.py
+++ SFV_ai/data_capture.py
@@ -12,7 +12,7 @@ def screen_record():
         all_data.append(printscreen)
         print(len(all_data))
         print(all_data[-1].shape)
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        # cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         k = cv2.waitKey(25) & 0xFF
         if k == ord('x'):
             cv2.destroyAllWindows()
@@ -22,4 +22,5 @@ def screen_record():
         print(concat_data.shape)
 
 if __name__ == '__main__':
+    time.sleep(10)
     screen_record()
