5226fe67c1a3fdc86c6adda91ff2364c14d27327
cdesilv1 <chris.desilva_@outlook.com>
2019-01-14 12:22:53 -0700

    changed data_capture

--- SFV_ai/data_capture.py
+++ SFV_ai/data_capture.py
@@ -11,7 +11,7 @@ def screen_record():
         printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)).convert('L'))
         all_data.append(printscreen)
         cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
-        k = cv2.waitKey(0) & 0xFF
+        k = cv2.waitKey(10000) & 0xFF
         if k == ord('x'):
             cv2.destroyAllWindows()
             concat_data = np.stack(all_data, axis=2)
