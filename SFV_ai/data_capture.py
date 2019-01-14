2cb70d53f3bb32711ad60b522dc8ee24dac60d7f
cdesilv1 <chris.desilva_@outlook.com>
2019-01-14 12:06:27 -0700

    changed data_capture

--- SFV_ai/data_capture.py
+++ SFV_ai/data_capture.py
@@ -2,15 +2,19 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
+import gc 
 
 def screen_record():
     all_data = np.array([])
     while(True):
         # 800x600 windowed mode
         printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)).convert('L'))
+        all_data.append(printscreen)
         # cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('x'):
             cv2.destroyAllWindows()
+            concat_data = np.stack(all_data, axis=2)
+            print(concat_data.shape)
             break
 
 if __name__ == '__main__':
