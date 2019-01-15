4cdd981bc66e489e7fdcc5aaa54aac4aa1809930
cdesilv1 <chris.desilva_@outlook.com>
2019-01-14 19:25:42 -0700

    deleted some print statements

--- SFV_ai/data_capture.py
+++ SFV_ai/data_capture.py
@@ -26,7 +26,6 @@ class screen_record_thread(threading.Thread):
             try:
                 # print('trying')
                 stop_triggered = self.in_q.get(True, 0.0167)
-                print('-------------------------------stop_triggered----------------------------')
                 concat_data = np.stack(self.all_data, axis=2)
                 print('data concatenated')
                 np.save('sfv_{}.npy'.format(unix_time_millis(datetime.datetime.now())), concat_data)
